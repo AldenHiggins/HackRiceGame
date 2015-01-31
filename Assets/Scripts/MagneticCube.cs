be68cfec5f0b3bf7bf4a579861a739db79554813
Hadi Tabani <haditabani5@gmail.com>
2015-01-31 14:50:47 -0800

    Added all the previous files. Ready to work on the Magnet cube now

---
+++ Assets/Scripts/MagneticCube.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagneticCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Projectiles.cs
+++ Assets/Scripts/Projectiles.cs
@@ -10,9 +10,12 @@ public class Projectiles : MonoBehaviour {
 
 	public GameObject cubeProjectile;
 	public GameObject player;
+	public GameObject cubeMagnet;
 
 	private GameObject cubeProjectileObject;
 	private bool previousBDown = false;
+	private bool previousXDown = false;
+	private bool previousYDown = false;
 
 	// Update is called once per frame
 	void Update () {
@@ -38,6 +41,56 @@ public class Projectiles : MonoBehaviour {
 		}
 
 		previousBDown = bPressed;
+
+		//Bomb Projectile
+		bool xPressed = OVRGamepadController.GPC_GetButton(OVRGamepadController.Button.X);
+		
+		// Create projectile
+		if (xPressed && !previousXDown)
+		{
+			//cubeProjectileObject = (GameObject) Instantiate (cubeBomb);
+		}
+		// Animate projectile in front of player
+		else if (xPressed)
+		{
+			print ("X being pressed");
+			cubeProjectileObject.transform.position = player.transform.position + 2 * player.transform.forward;
+			cubeProjectileObject.transform.Rotate (new Vector3(15, 30, 35) * Time.deltaTime);
+		}
+		// Fire projectile
+		else if (!xPressed && previousXDown)
+		{
+			print ("X released");
+			cubeProjectileObject.rigidbody.velocity += 8 * player.transform.forward;
+		}
+		
+		previousXDown = xPressed;
+
+
+		// Attaching the Y button to Magnetic Cube
+		bool yPressed = OVRGamepadController.GPC_GetButton(OVRGamepadController.Button.Y);
+
+		// Create projectile
+		if (yPressed && !previousYDown)
+		{
+			cubeProjectileObject = (GameObject) Instantiate (cubeMagnet);
+		}
+		// Animate projectile in front of player
+		else if (yPressed)
+		{
+			print ("Y being pressed");
+			cubeProjectileObject.transform.position = player.transform.position + 2 * player.transform.forward;
+			cubeProjectileObject.transform.Rotate (new Vector3(15, 30, 35) * Time.deltaTime);
+		}
+		// Fire projectile
+		else if (!yPressed && previousYDown)
+		{
+			print ("Y released");
+			cubeProjectileObject.rigidbody.velocity += 8 * player.transform.forward;
+		}
+
+		previousYDown = yPressed;
+
 	}
 
 }
