70e126b24094ad2c5987585e66ef567a57a06cef
Alden Higgins <johnaldenhiggins@gmail.com>
2015-01-31 22:51:11 -0600

    new Scene with space background and winnning circle

---
+++ Assets/Scripts/AldenTest/EscalatorMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscalatorMove : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.name == "CubeBullet(Clone)")
+		{
+			print ("Entered this collider");
+			print (transform.forward);
+			other.rigidbody.velocity += transform.forward * 4f;
+		}
+
+	}
+}
---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndGame : MonoBehaviour 
+{
+	public Text uiTextLeft;
+	public Text uiTextRight;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.name == "CubeBullet(Clone)")
+		{
+			print ("Game over bro");
+			uiTextLeft.text = "YOU WIN!!!!!";
+			uiTextRight.text = "YOU WIN!!!!";
+		}
+	}
+}
