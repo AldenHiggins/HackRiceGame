7501a253d501a44eea20874acde4d3b022df1e58
Alden Higgins <johnaldenhiggins@gmail.com>
2015-02-01 03:08:18 -0600

    Game Logic and UI, limited amount of push and pull

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour 
+{
+	public int startingPull;
+	public int startingPush;
+
+	public Text pullTextLeft;
+	public Text pushTextLeft;
+
+	public Text pullTextRight;
+	public Text pushTextRight;
+
+	private int pullBlocksLeft;
+	private int pushBlocksLeft;
+	// Use this for initialization
+	void Start () 
+	{
+		pullBlocksLeft = startingPull;
+		pushBlocksLeft = startingPush;
+
+		pullTextRight.text = "" + startingPull;
+		pullTextLeft.text = "" + startingPull;
+
+		pushTextLeft.text = "" + startingPush;
+		pushTextRight.text = "" + startingPush;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void placePushBlock()
+	{
+		pushBlocksLeft--;
+		pushTextLeft.text = "" + pushBlocksLeft;
+		pushTextRight.text = "" + pushBlocksLeft;
+	}
+
+	public void placePullBlock()
+	{
+		pullBlocksLeft--;
+		pullTextRight.text = "" + pullBlocksLeft;
+		pullTextLeft.text = "" + pullBlocksLeft;
+	}
+
+	public int getPushBlocksLeft()
+	{
+		return pushBlocksLeft;
+	}
+
+	public int getPullBlocksLeft()
+	{
+		return pullBlocksLeft;
+	}
+}
+
