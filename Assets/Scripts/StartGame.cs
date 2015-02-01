9ee17c91232292e5e435dffe2d6f44659665d9b7
Alden Higgins <johnaldenhiggins@gmail.com>
2015-02-01 11:36:44 -0600

    Working starting screen

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		bool startPressed = OVRGamepadController.GPC_GetButton(OVRGamepadController.Button.Start);
+
+		if (startPressed)
+		{
+			Application.LoadLevel("AldenRoom");
+		}
+	}
+}
