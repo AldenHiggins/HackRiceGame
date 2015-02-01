bebd3e65fb6667ed74659ea63226a9c8335d8dd4
Hadi Tabani <haditabani5@gmail.com>
2015-01-31 22:52:56 -0600

    Changed the strength of the push and the pull and added transparent spheres.

--- Assets/Scripts/MagneticCube.cs
+++ Assets/Scripts/MagneticCube.cs
@@ -16,13 +16,13 @@ public class MagneticCube : MonoBehaviour {
 		Rigidbody rigidbody;
 
 		//print ("Made a magnetic cube.");
-		colliders = Physics.OverlapSphere (transform.position , 20f);
+		colliders = Physics.OverlapSphere (transform.position ,10f);
 		foreach (Collider collider in colliders)
 		{
 			//print ("GameObject is  " + collider.gameObject.name);
 			if (collider.gameObject.name == "CubeBullet(Clone)") {
 				float distance = Vector3.Distance (collider.gameObject.transform.position, transform.position);
-				if (distance > 2f) {
+				if (distance > 1f) {
 
 
 					rigidbody = (Rigidbody)collider.gameObject.GetComponent (typeof(Rigidbody));
@@ -30,7 +30,7 @@ public class MagneticCube : MonoBehaviour {
 						continue;
 					}
 					//print ("Found a rigidbody");
-					rigidbody.AddExplosionForce (100f * -1, transform.position, 20f);
+					rigidbody.AddExplosionForce (10f * -1, transform.position, 10f);
 				}
 			}
 		}
--- Assets/Scripts/PushCube.cs
+++ Assets/Scripts/PushCube.cs
@@ -14,22 +14,20 @@ public class PushCube : MonoBehaviour {
 		Rigidbody rigidbody;
 
 		//print ("Made a magnetic cube.");
-		colliders = Physics.OverlapSphere (transform.position , 10f);
+		colliders = Physics.OverlapSphere (transform.position , 8f);
 		foreach (Collider collider in colliders)
 		{
 			//print ("GameObject is  " + collider.gameObject.name);
 			if (collider.gameObject.name == "CubeBullet(Clone)") {
-				float distance = Vector3.Distance (collider.gameObject.transform.position, transform.position);
-				if (distance < 10f) {
+//				float distance = Vector3.Distance (collider.gameObject.transform.position, transform.position);
 
-
-					rigidbody = (Rigidbody)collider.gameObject.GetComponent (typeof(Rigidbody));
-					if (rigidbody == null) {
-						continue;
-					}
-					//print ("Found a rigidbody");
-					rigidbody.AddExplosionForce (100f * 1, transform.position, 20f);
+				rigidbody = (Rigidbody)collider.gameObject.GetComponent (typeof(Rigidbody));
+				if (rigidbody == null) {
+					continue;
 				}
+				//print ("Found a rigidbody");
+				rigidbody.AddExplosionForce (10f * 1, transform.position, 8f);
+
 			}
 		}
 	}
